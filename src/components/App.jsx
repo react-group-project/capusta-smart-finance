6261c959a100b2b8bf3d4a02a0efc5b2edb16410
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-01-29 22:09:25 +0200

    Fix Expense amount, add select by category

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,129 @@
 import Header from './Header/Header.jsx';
 import AppToastContainer from './AppToastContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginThunk } from 'redux/auth/auth.thunk.js';
+import {
+    addExpenseThunk,
+    addIncomeThunk,
+    getAllTransactionsThunk,
+} from 'redux/transactions/transactions.thunk.js';
+import { useState, useEffect } from 'react';
+import { selectUserBalance } from 'redux/user/user.selectors';
+import { getPeriodDataThunk } from '../redux/transactions/transactions.thunk';
+import {
+    selectIncomesPeriodByCategory,
+    selectExposesPeriodByCategory,
+} from '../redux/transactions/transactions.selectors';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
+    const dispatch = useDispatch();
+    const balance = useSelector(selectUserBalance);
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('Alcohol');
+    const [incomeDescription, setIncomeDescription] = useState('');
+    const [incomeAmount, setIncomeAmount] = useState(0);
+    const [incomeCategory, setIncomeCategory] = useState('Salary');
 
-      <AppToastContainer />
-    </>
-  );
+    // Запити для отримання данних за період по конкретній категорії
+    const periodIncomeData = useSelector(state =>
+        selectIncomesPeriodByCategory(state, 'Salary')
+    );
+    const periodExpenseData = useSelector(state =>
+        selectExposesPeriodByCategory(state, 'Alcohol')
+    );
+    // ========================================
+
+    const clickHandler = () => {
+        dispatch(
+            loginThunk({
+                email: 'goit.test6.user@test.com',
+                password: 'qwerty123',
+            })
+        )
+            .unwrap()
+            .then(() => {
+                dispatch(getAllTransactionsThunk());
+                dispatch(getPeriodDataThunk({ date: '2023-01' }));
+            });
+    };
+
+    const addExpenseHandler = () => {
+        dispatch(
+            addExpenseThunk({
+                description,
+                category,
+                amount,
+                date: '2023-01-29',
+            })
+        );
+    };
+    const addIncomeHandler = () => {
+        dispatch(
+            addIncomeThunk({
+                description: incomeDescription,
+                category: incomeCategory,
+                amount: Number(incomeAmount),
+                date: '2023-01-29',
+            })
+        );
+    };
+
+    useEffect(() => {
+        console.log(periodIncomeData, periodExpenseData);
+    }, [periodIncomeData, periodExpenseData]);
+
+    return (
+        <>
+            <Header />
+
+            <button onClick={clickHandler}>Login</button>
+
+            <div>{balance}</div>
+
+            <div>
+                <input
+                    type="text"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <select
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}
+                >
+                    <option calue="Alcohol">Alcohol</option>
+                    <option calue="Entertainment">Entertainment</option>
+                    <option calue="Health">Health</option>
+                </select>
+                <input
+                    type="number"
+                    value={amount}
+                    onChange={e => setAmount(e.target.value)}
+                />
+                <button onClick={addExpenseHandler}>Add Expenses</button>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    value={incomeDescription}
+                    onChange={e => setIncomeDescription(e.target.value)}
+                />
+                <select
+                    value={incomeCategory}
+                    onChange={e => setIncomeCategory(e.target.value)}
+                >
+                    <option calue="Salary">Salary</option>
+                    <option calue="Add. income">Add. income</option>
+                </select>
+                <input
+                    type="number"
+                    value={incomeAmount}
+                    onChange={e => setIncomeAmount(e.target.value)}
+                />
+                <button onClick={addIncomeHandler}>Add Incomes</button>
+            </div>
+
+            <AppToastContainer />
+        </>
+    );
 };
--- src/redux/transactions/transactions.thunk.js
+++ src/redux/transactions/transactions.thunk.js
@@ -1,234 +1,244 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
-  checkForAutorizationError,
-  processingError,
-  convertIncomeListToEng,
-  convertExpenseListToEng,
-  convertIncomeValueToRu,
-  convertExpenseValueToRu,
-  convertExpenseValueToEng,
-  convertIncomeValueToEng,
-  convertMonthToEng,
-  successNotification,
+    checkForAutorizationError,
+    processingError,
+    convertIncomeListToEng,
+    convertExpenseListToEng,
+    convertIncomeValueToRu,
+    convertExpenseValueToRu,
+    convertExpenseValueToEng,
+    convertIncomeValueToEng,
+    convertMonthToEng,
+    successNotification,
 } from 'helpers';
 import {
-  getIncomeCategoriesService,
-  getExpenseCategoriesService,
-  addExpenseService,
-  addIncomeService,
-  getExpenseService,
-  getIncomeService,
-  deleteTransactionService,
-  getPeriodDataService,
+    getIncomeCategoriesService,
+    getExpenseCategoriesService,
+    addExpenseService,
+    addIncomeService,
+    getExpenseService,
+    getIncomeService,
+    deleteTransactionService,
+    getPeriodDataService,
 } from 'services/transactions.service';
 
 // Get all expenses list
 export const getExpenseThunk = createAsyncThunk(
-  'transactions/getExpense',
-  async (_, { rejectWithValue }) => {
-    try {
-      const data = await getExpenseService();
+    'transactions/getExpense',
+    async (_, { rejectWithValue }) => {
+        try {
+            const data = await getExpenseService();
 
-      const convertedData = data.expenses.map(item => ({
-        ...item,
-        category: convertExpenseValueToEng(item.category),
-      }));
+            const convertedData = data.expenses.map(item => ({
+                ...item,
+                amount: item.amount * -1,
+                category: convertExpenseValueToEng(item.category),
+            }));
 
-      const convertedMonth = Object.keys(data.monthsStats).map(key => {
-        return { ...convertMonthToEng(key), amount: data.monthsStats[key] };
-      });
+            const convertedMonth = Object.keys(data.monthsStats).map(key => {
+                return {
+                    ...convertMonthToEng(key),
+                    amount: data.monthsStats[key],
+                };
+            });
 
-      return { data: convertedData, monthsStats: convertedMonth };
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return { data: convertedData, monthsStats: convertedMonth };
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Add expense to list
 export const addExpenseThunk = createAsyncThunk(
-  'transactions/addExpense',
-  async (body, { rejectWithValue, dispatch }) => {
-    try {
-      const data = await addExpenseService({
-        ...body,
-        category: convertExpenseValueToRu(body.category),
-      });
+    'transactions/addExpense',
+    async (body, { rejectWithValue, dispatch }) => {
+        try {
+            const data = await addExpenseService({
+                ...body,
+                category: convertExpenseValueToRu(body.category),
+            });
 
-      dispatch(getExpenseThunk());
-      successNotification(
-        `Expense "${data.transaction.description}" is added.`
-      );
-      return data;
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            dispatch(getExpenseThunk());
+            successNotification(
+                `Expense "${data.transaction.description}" is added.`
+            );
+            return data;
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Get all incomes list
 export const getIncomeThunk = createAsyncThunk(
-  'transactions/getIncome',
-  async (_, { rejectWithValue }) => {
-    try {
-      const data = await getIncomeService();
-      const convertedData = data.incomes.map(item => ({
-        ...item,
-        category: convertIncomeValueToEng(item.category),
-      }));
-      const convertedMonth = Object.keys(data.monthsStats).map(key => {
-        return { ...convertMonthToEng(key), amount: data.monthsStats[key] };
-      });
+    'transactions/getIncome',
+    async (_, { rejectWithValue }) => {
+        try {
+            const data = await getIncomeService();
+            const convertedData = data.incomes.map(item => ({
+                ...item,
+                category: convertIncomeValueToEng(item.category),
+            }));
+            const convertedMonth = Object.keys(data.monthsStats).map(key => {
+                return {
+                    ...convertMonthToEng(key),
+                    amount: data.monthsStats[key],
+                };
+            });
 
-      return { data: convertedData, monthsStats: convertedMonth };
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return { data: convertedData, monthsStats: convertedMonth };
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Add income to list
 export const addIncomeThunk = createAsyncThunk(
-  'transactions/addIncome',
-  async (body, { rejectWithValue, dispatch }) => {
-    try {
-      const data = await addIncomeService({
-        ...body,
-        category: convertIncomeValueToRu(body.category),
-      });
+    'transactions/addIncome',
+    async (body, { rejectWithValue, dispatch }) => {
+        try {
+            const data = await addIncomeService({
+                ...body,
+                category: convertIncomeValueToRu(body.category),
+            });
 
-      dispatch(getIncomeThunk());
-      successNotification(`Income "${data.transaction.description}" is added.`);
+            dispatch(getIncomeThunk());
+            successNotification(
+                `Income "${data.transaction.description}" is added.`
+            );
 
-      return data;
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return data;
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Delete transaction by id
 export const deleteTransactionThunk = createAsyncThunk(
-  'transactions/deleteTransaction',
-  async (transactionId, { rejectWithValue, dispatch }) => {
-    try {
-      const data = await deleteTransactionService(transactionId);
+    'transactions/deleteTransaction',
+    async (transactionId, { rejectWithValue, dispatch }) => {
+        try {
+            const data = await deleteTransactionService(transactionId);
 
-      dispatch(getAllTransactionsThunk());
-      successNotification(`Transaction deleted.`);
+            dispatch(getAllTransactionsThunk());
+            successNotification(`Transaction deleted.`);
 
-      return data;
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return data;
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // QUESTION: 1) Спитати, як викликати цю функцію. Можно лі взагалі не викликати при перемиканнкі income expenses на home page, Тому що при видаленні чи додаванні транзакціі робиться запит на сервер. 2) робити promise.all для dispatch чи для звичаних запиті?
 // Get all transactions
 export const getAllTransactionsThunk = createAsyncThunk(
-  'transactions/getAllTransactions',
-  async (_, { rejectWithValue, dispatch }) => {
-    try {
-      const result = await Promise.all([
-        dispatch(getExpenseThunk()),
-        dispatch(getIncomeThunk()),
-      ]);
+    'transactions/getAllTransactions',
+    async (_, { rejectWithValue, dispatch }) => {
+        try {
+            const result = await Promise.all([
+                dispatch(getExpenseThunk()),
+                dispatch(getIncomeThunk()),
+            ]);
 
-      return { expenses: result[0].payload, incomes: result[1].payload };
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return { expenses: result[0].payload, incomes: result[1].payload };
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Get income category in eng
 export const getIncomeCategoriesThunk = createAsyncThunk(
-  'transactions/getIncomeCategories',
-  async (_, { rejectWithValue }) => {
-    try {
-      const data = await getIncomeCategoriesService();
+    'transactions/getIncomeCategories',
+    async (_, { rejectWithValue }) => {
+        try {
+            const data = await getIncomeCategoriesService();
 
-      return convertIncomeListToEng(data);
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return convertIncomeListToEng(data);
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Get expense category in eng
 export const getExpenseCategoriesThunk = createAsyncThunk(
-  'transactions/getExpenseCategories',
-  async (_, { rejectWithValue }) => {
-    try {
-      const data = await getExpenseCategoriesService();
+    'transactions/getExpenseCategories',
+    async (_, { rejectWithValue }) => {
+        try {
+            const data = await getExpenseCategoriesService();
 
-      return convertExpenseListToEng(data);
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return convertExpenseListToEng(data);
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // Get data for a specified period
 export const getPeriodDataThunk = createAsyncThunk(
-  'transactions/detPeriodData',
-  async (body, { rejectWithValue }) => {
-    try {
-      const data = await getPeriodDataService(body);
-      const expenses = convertTransaction(
-        data.expenses.expenseTotal,
-        data.expenses.expensesData
-      );
-      const incomes = convertTransaction(
-        data.incomes.incomeTotal,
-        data.incomes.incomesData
-      );
+    'transactions/getPeriodData',
+    async (body, { rejectWithValue }) => {
+        try {
+            const data = await getPeriodDataService(body);
+            const expenses = convertTransaction(
+                data.expenses.expenseTotal,
+                data.expenses.expensesData
+            );
+            const incomes = convertTransaction(
+                data.incomes.incomeTotal,
+                data.incomes.incomesData
+            );
 
-      return { incomes, expenses };
-    } catch (error) {
-      checkForAutorizationError(error.response?.status);
-      return rejectWithValue(processingError(error));
+            return { incomes, expenses };
+        } catch (error) {
+            checkForAutorizationError(error.response?.status);
+            return rejectWithValue(processingError(error));
+        }
     }
-  }
 );
 
 // TODO: перенести до допоміжних функцій
 function convertTransaction(total, data) {
-  return {
-    total: total,
-    data: Object.keys(data).reduce((acc, key) => {
-      return {
-        ...acc,
-        [convertExpenseValueToEng(key) || convertIncomeValueToEng(key)]: {
-          total: data[key].total,
-          list: convertObjectToDescriptionList(data[key]),
-        },
-      };
-    }, {}),
-  };
+    return {
+        total: total,
+        data: Object.keys(data).reduce((acc, key) => {
+            return {
+                ...acc,
+                [convertExpenseValueToEng(key) || convertIncomeValueToEng(key)]:
+                    {
+                        total: data[key].total,
+                        list: convertObjectToDescriptionList(data[key]),
+                    },
+            };
+        }, {}),
+    };
 }
 
 function convertObjectToDescriptionList(obj) {
-  return Object.keys(obj).reduce(
-    (acc, key) =>
-      key === 'total'
-        ? acc
-        : [
-            ...acc,
-            {
-              description: key,
-              amount: obj[key],
-            },
-          ],
-    []
-  );
+    return Object.keys(obj).reduce(
+        (acc, key) =>
+            key === 'total'
+                ? acc
+                : [
+                      ...acc,
+                      {
+                          description: key,
+                          amount: obj[key],
+                      },
+                  ],
+        []
+    );
 }
