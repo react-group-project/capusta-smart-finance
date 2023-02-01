fe66d2a440ba415672c1bd57d960f9cbdc4aa5a0
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-02-01 09:22:44 +0200

    Add Main title and styles

--- src/pages/Auth/Auth.page.jsx
+++ src/pages/Auth/Auth.page.jsx
@@ -1,17 +1,31 @@
-import { Container } from 'components/Common/Container/Container.styled';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { BackgroundAuth } from '../../components/Common/BackgroundAuth/BackgroundAuth.styled';
+import { Box } from 'components/Box/Box.styled';
+import { BackgroundAuth } from 'components/Common/BackgroundAuth/BackgroundAuth.styled';
+import {
+  AuthContainer,
+  AuthOutlet,
+  AuthSubTitle,
+  AuthTitle,
+  AuthTitleLayout,
+} from './Auth.styled';
 
 export default function AuthPage() {
   return (
-    <BackgroundAuth>
-      <Container>
-        <h1>Kapu$ta</h1>
-        <Suspense fallback={null}>
-          <Outlet />
-        </Suspense>
-      </Container>
-    </BackgroundAuth>
+    <Box as="section">
+      <BackgroundAuth>
+        <AuthContainer>
+          <AuthTitleLayout>
+            <AuthTitle as="h1">Kapu$ta</AuthTitle>
+            <AuthSubTitle as="h2">Smart finance</AuthSubTitle>
+          </AuthTitleLayout>
+          <AuthOutlet>
+            <Suspense fallback={null}>
+              <Outlet />
+            </Suspense>
+          </AuthOutlet>
+        </AuthContainer>
+      </BackgroundAuth>
+    </Box>
   );
 }
